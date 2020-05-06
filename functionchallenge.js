caf14ea061471867a38f11713ada82987b1f2681
SergioJss <56595399+SergioJss@users.noreply.github.com>
2020-05-05 23:37:15 -0400

    testing stuff after a long time

--- functionchallenge.js
+++ functionchallenge.js
@@ -7,20 +7,22 @@ console.log("Hello Challenge");
 // fortuneTeller("roger", "bali", "29", "tesla", "purple", "6")
 // expect to return a fortune using all arguments
 
-fortuneTeller("roger", "bali", "29", "tesla", "purple", "6")
-
-function fortuneTeller(name, place, age, car, color, favnum){
-
-    let fortune = name + "is a gamer, he is" + age + "he was swimming in a " + place + ", he will win a " + car + ". his wife's eyes will be " + color + ", he's gonna have " + favnum + "million dollars."
-
-    return alert(fortune)
-}
 
 let person = prompt("Please enter your name");
 let personAge = prompt("Please enter your age");
 let personPlace = prompt("Please enter your Favorite place");
-let personCar = prompt("Please enter your favorite var");
+let personCar = prompt("Please enter your favorite car");
 let personColor = prompt("Please enter your favorite color");
 let personNumber = prompt("Please enter your favotie number");
+
+fortuneTeller( person , personAge , personPlace, personCar, personColor, personNumber)
+
+function fortuneTeller(name, age, place, car, color, favnum){
+
+    let fortune = name + " is a gamer, he is " + age + ", he was swimming in a " + place + ", he will win a " + car + ". his wife's eyes will be " + color + ", he's gonna have " + favnum + " million dollars."
+
+    return alert(fortune)
+}
+
 
 fortuneTeller
--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 console.log("loops connected");
 
-let products = [ "cereal","milk", "waterbottle", "cheese", "soap"]
+let products = [ " cereal", " milk", " waterbottle", " cheese", " soap"]
 let cart = [];
 
 let userSelection = prompt("please shop our products:" + products)
@@ -14,35 +14,28 @@ let indexToRemove;
 
 
 for (let index = 0; index < 6; index++) {
-     console.log(products [index]);
-     debugger
-     if(userSelection === products[index]) {
-         indexToRemove = index
-     }
-     products.splicer (indexToRemove, 0);
- }
+    
+     if(userSelection === "cereal") {
+        indexToRemove = 0;
+    } else if(userSelection === "milk") {
+        indexToRemove = 1;
+    } else if(userSelection === "waterbottle") {
+        indexToRemove = 2;
+    } else if(userSelection === "cheese") {
+        indexToRemove = 3;
+    } else if(userSelection === "soap") {
+        indexToRemove = 4;
+    } else {
+        alert("we don't have what you want at the moment.")
+    }
 
+products.splice( indexToRemove, 1)
 
-
-
-// if(userSelection === "milk") {
-//     indexToRemove = 1;
-// } else if(userSelection === "cereal") {
-//     indexToRemove = 0;
-// } else if(userSelection === "waterbottle") {
-//     indexToRemove = 2;
-// } else if(userSelection === "soap") {
-//     indexToRemove = 4;
-// } else if(userSelection === "cheese") {
-//     indexToRemove = 3;
-// } else {
-//     alert("we don't have what you want at the moment.")
-// }
-
-// products.splice( indexToRemove, 1)
-
-// alert(prompt("Please shop out products:"
-//  + products));
-
-
- 
+//          if(userSelection === products[index]) {
+//         indexToRemove = index
+//     }
+//     products.splice(indexToRemove, 1);
+    alert(prompt("Please shop out products:"
+ + products));
+debugger
+}
