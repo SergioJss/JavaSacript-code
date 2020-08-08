bd0098a2764a3f2d8bfb0ece3ed51ec905fa778d
SergioJss <56595399+SergioJss@users.noreply.github.com>
2020-08-07 22:51:19 -0400

    jodiendo

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 console.log("loops connected");
 
-let products = [ " cereal", " milk", " waterbottle", " cheese", " soap"]
+let products = [ "cereal", "milk", "waterbottle", "cheese", "soap"]
 let cart = [];
 
-let userSelection = prompt("please shop our products:" + products)
+let userSelection = prompt("please shop our products: " + products)
 
 cart.push(userSelection);
 
@@ -15,26 +15,26 @@ let indexToRemove;
 
 for (let index = 0; index < 6; index++) {
     
-     if(userSelection === "cereal") {
-        indexToRemove = 0;
-    } else if(userSelection === "milk") {
-        indexToRemove = 1;
-    } else if(userSelection === "waterbottle") {
-        indexToRemove = 2;
-    } else if(userSelection === "cheese") {
-        indexToRemove = 3;
-    } else if(userSelection === "soap") {
-        indexToRemove = 4;
-    } else {
-        alert("we don't have what you want at the moment.")
-    }
-
-products.splice( indexToRemove, 1)
-
-//          if(userSelection === products[index]) {
-//         indexToRemove = index
+//      if(userSelection === "cereal") {
+//         indexToRemove = 0;
+//     } else if(userSelection === "milk") {
+//         indexToRemove = 1;
+//     } else if(userSelection === "waterbottle") {
+//         indexToRemove = 2;
+//     } else if(userSelection === "cheese") {
+//         indexToRemove = 3;
+//     } else if(userSelection === "soap") {
+//         indexToRemove = 4;
+//     } else {
+//         alert("we don't have what you wantt the moment.")
 //     }
-//     products.splice(indexToRemove, 1);
+
+// products.splice( indexToRemove, 1)
+
+         if(userSelection === products[index]) {
+        indexToRemove = userSelection
+    }
+    products.splice(indexToRemove, 1);
     alert(prompt("Please shop out products:"
  + products));
 debugger
